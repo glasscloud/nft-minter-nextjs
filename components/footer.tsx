51fdad1bf0672fbea5928e1938cac51a255a9f1e
Glasscloud <glasscloudweb@protonmail.com>
2021-11-07 14:01:47 -0500

    Styled footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,8 +3,8 @@ type FooterProps = {
 }
 
 export const Footer: React.FC<FooterProps> = ({ children, className }) => (
-  <footer className={className}>
-    {children}
+  <footer className={"flex align-items-center justify-items-center flex-wrap bg-transparent p-6"}>
+    <p className={"block mt-4 lg:inline-block lg:mt-0 text-white"}>Made by some 🐹</p>
   </footer>
 )
 
